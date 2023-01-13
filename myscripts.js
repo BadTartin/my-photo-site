9ae4f821049e1ecc870da8dfb9c9652634b664af
Tad Martin <tadfmartin@gmail.com>
2023-01-12 21:51:50 -0500

    added new photo added functionality to pick main image by clicking on carousel images

--- myscripts.js
+++ myscripts.js
@@ -1,31 +1,18 @@
-document.querySelector('')
+const display = document.querySelector('#display');
+display.addEventListener('click', function (){
+    console.log(this);
+})
 
+const mini = document.querySelectorAll(".mini");
 
-document.querySelectorAll('.mini').forEach(item => {
-    item.addEventListener('click', event => {
-      //handle click
+mini.forEach(item => {
+    item.addEventListener('click', function() {
+        newSrc = this.src;
+        changeImg();
     })
   })
 
+function changeImg () {
+   display.src = newSrc;
+}
 
-//   window.onload = function() {
-//     var imgs = document.getElementsByTagName('img');
-//     for(var i = 0; i < imgs.length; i++) {
-//         var img = imgs[i];
-//         img.onclick = function() {
-//             newSrc = this.src; //this = reference to image that fired onclick
-//             focus = document.getElementById('focus');
-//             focus.src = newSrc; //no quotes for variable references!
-//         }
-//     }
-// }
-
-// var image = document.getElementById('myImage');
-
-// image.addEventListener('click', function(){
-//     changeImage();
-// });
-
-// function changeImage(){
-//     image.src = './squid-dab2.gif';
-// }
